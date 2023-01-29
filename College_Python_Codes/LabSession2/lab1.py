075e480417085460723f4106a5ec8e5a9ed0c765
Akash11gavhane <akash1106gavhane@gmail.com>
2023-01-29 12:37:02 +0530

    oops-part 2 new file

---
+++ College_Python_Codes/LabSession2/lab1.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read.csv("C:\Users\AKASH\Downloads\Business-employment-data-september-2022-quarter-csv\Business-employment-data-september-2022-quarter-csv.csv")
+
+print(df)
